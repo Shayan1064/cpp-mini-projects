2269a3b48546b1c1f46afb41f4df10b9d29d807d
Shayan1064 <gamingfanatics2115@gmail.com>
2025-05-06 22:52:04 +0500

    Temprature converter

---
+++ TempratureConverter.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    float c;
+    cout << "Enter temperature in Celsius: ";
+    cin >> c;
+
+    float f = (c * 9 / 5) + 32;
+    cout << "Fahrenheit: " << f << endl;
+
+    return 0;
+}
